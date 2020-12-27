09fb4c498e197960a591849cb50911347194a41b
radwaahmed2132000 <56734728+radwaahmed2132000@users.noreply.github.com>
2020-12-27 16:44:08 +0200

    try

--- moves.asm
+++ moves.asm
@@ -1,3 +1,26 @@
+Drawpaddel MACRO BALLX2,BALLY2,BALLwidth,Ballheight
+        LOCAL Draw1
+        mov cx,BALLX2
+        mov dx,BALLY2
+ Draw1:  mov al,0fh
+        mov ah,0ch
+        int 10h
+        inc cx
+        mov ax,cx
+        sub ax,BALLX2
+        cmp ax,BALLwidth
+        jng Draw1
+        mov cx,BALLX2
+        inc dx
+        mov ax,dx
+        sub ax,BALLY2
+        cmp ax,Ballheight
+        jng Draw1
+  
+
+        
+    
+ENDM
  .MODEL SMALL
     .STACK 64
 
@@ -18,6 +41,16 @@
 	BALL_VELOCITY_Y DW 02h ;Y (vertical) velocity of the ball
    VAR1 DW ?
    VAR2 DW ?
+    ;;;;
+    PADDLE_LEFT_X DW 0Ah
+	PADDLE_LEFT_Y DW 0Ah
+	
+	PADDLE_RIGHT_X DW 130h
+	PADDLE_RIGHT_Y DW 0Ah
+	
+	PADDLE_WIDTH DW 05h
+	PADDLE_HEIGHT DW 1Fh
+    PADDEL_VECLOITY DW 05H
 
 
 .CODE 
@@ -33,23 +66,26 @@
           mov cx,3h  
           mov bp,0h
                         Drawnewball: 
-                                
+                            ;six balls    
                             lea si,BALLX
                             lea di,BALLY
                         try: 
-                         mov VAR1,si
-                         mov var2,di
-                         CALL MOVE_BALL 
+                         mov VAR1,si ; store index  position of x
+                         mov var2,di ; store index position of y
+                         CALL MOVE_BALL  ; here si,di changes so i need to know where was my postion so i can get it from var1,var2
                     				  
-                                 mov si,var1
-                                 mov di,var2
-                                 add si,2h
-                                 add di,2h
-                             inc bp
-                             cmp bp,6h               
+                                 mov si,var1 ; index position of x
+                                 mov di,var2 ; index postion of y
+                                 add si,2h ; next index of x
+                                 add di,2h ;next index of y
+                             inc bp  ; counter
+                             cmp bp,6h  ;size of array              
                              jl try
-                           
-                         mov bp,0           
+							 ;CALL MOve_Paddel 
+                             ; Drawpaddel PADDLE_LEFT_X,PADDLE_LEFT_Y,PADDLE_WIDTH,PADDLE_HEIGHT ; YOU CAN stop it to see the balls only
+                             ;Drawpaddel PADDLE_RIGHT_X,PADDLE_RIGHT_Y,PADDLE_WIDTH,PADDLE_HEIGHT ; YOU STop it to see the balls 
+							 ;CALL MOve_Paddel
+                         mov bp,0           ;draw
                          lea si,BALLX
                          lea di,BALLY
                         try1: 
@@ -160,7 +196,7 @@
 		RET
 	DRAW_BALL ENDP
 	
-	CLEAR_SCREEN PROC NEAR
+CLEAR_SCREEN PROC NEAR
 			MOV AH,00h ;set the configuration to video mode
 			MOV AL,13h ;choose the video mode
 			INT 10h    ;execute the configuration 
@@ -171,7 +207,88 @@
 			INT 10h    ;execute the configuration
 			
 			RET
-	CLEAR_SCREEN ENDP
+CLEAR_SCREEN ENDP
+
+MOve_Paddel PROC
+    mov ah,01h
+    int 16h
+    JZ CHECK_RIGHT
+    mov ah,00h
+    int 16h
+    CMP AL,77h ;'w'
+    je MOVE_UP
+    CMP AL, 73h ;'s'
+    je MOVE_DOWN
+    
+   MOVE_UP:
+        MOV AX,PADDEL_VECLOITY
+        sub PADDLE_LEFT_Y,AX
+        MOV AX,WINDOW_BOUNDS
+        CMP PADDLE_LEFT_Y,AX
+        JL FIX_PADDEL
+        jmp CHECK_RIGHT
+          
+      
+   MOVE_DOWN:
+        MOV AX,PADDEL_VECLOITY
+        add PADDLE_LEFT_Y,AX
+        
+        mov ax,Window_height
+        sub ax,WINDOW_BOUNDS
+        sub ax,PADDLE_HEIGHT
+        cmp PADDLE_LEFT_Y,ax
+        jg FIX_PADDEL2
+        jmp CHECK_RIGHT
+          
+
+   
+    
+   FIX_PADDEL: mov ax,WINDOW_BOUNDS
+               mov PADDLE_LEFT_Y,ax
+                jmp CHECK_RIGHT
+   
+   FIX_PADDEL2: MOV PADDLE_LEFT_Y,AX
+    
+    CHECK_RIGHT:  
+    mov ah,01h
+    int 16h
+    jz close
+    mov ah,00h
+    int 16h
+    CMP AL,70h ;'p'
+    je MOVE_UP1
+    CMP AL, 6ch ;'l'
+    je MOVE_DOWN1
+    RET
+   MOVE_UP1:
+        MOV AX,PADDEL_VECLOITY
+        sub PADDLE_RIGHT_Y,AX
+        MOV AX,WINDOW_BOUNDS
+        CMP PADDLE_RIGHT_Y,AX
+        JL FIX_PADDEL1
+          RET
+      
+   MOVE_DOWN1:
+        MOV AX,PADDEL_VECLOITY
+        add PADDLE_RIGHT_Y,AX
+        
+        mov ax,Window_height
+        sub ax,WINDOW_BOUNDS
+        sub ax,PADDLE_HEIGHT
+        cmp PADDLE_RIGHT_Y,ax
+        jg FIX_PADDEL22
+        RET
+    FIX_PADDEL1: mov ax,WINDOW_BOUNDS
+               mov PADDLE_right_Y,ax
+                RET
+   
+    FIX_PADDEL22: MOV PADDLE_right_Y,AX
+
+    close:     
+       RET
+                
+    
+MOve_Paddel ENDP
 
 
 END MAIN
